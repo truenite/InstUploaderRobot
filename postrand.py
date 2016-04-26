cd66dfc8d0048314657187540351a5165892c9ca
Deco <diego.mendiburu@gmail.com>
2016-04-26 04:59:44 -0700

    script

---
+++ postrand.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+from os import walk
+from os import rename
+from pynstagram import client
+import random
+import logging
+import sys
+
+caption_extras = " #hashtag"
+username = "xxx"
+password = "xxx"
+
+
+origin = "./to_post/"
+posted_destination = "./posted/"
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    filename='./log.log',
+                    filemode='w')
+
+f = []
+for (dirpath, dirnames, filenames) in walk(origin):
+    f.extend(filenames)
+if len(filenames) is 0:
+    logging.warning('No more images to post')
+    sys.exit()
+image = random.choice(filenames)
+image_path = origin+image
+
+
+# Image caption comes from the filename
+# my_image.jpg -> My Image
+caption = image[0:image.find(".")].replace("_", " ").title()
+caption = caption+caption_extras
+
+pynstagram_client = client(username, password)
+pynstagram_client.upload(image_path, caption)
+
+# If image is not posted script will exit with exception
+# no need to verify posting
+
+logging.info("Posted: %s", image_path)
+
+posted_image_path = posted_destination+image
+
+try:
+    rename(image_path, posted_image_path)
+except OSError:
+    pass
+
+logging.info("Moved %s to: %s", image_path, posted_image_path)
